75e3b717da8cfbfdabdf26feb4cb202723738d7c
Mark Loeser <halcy0n@gentoo.org>
2006-01-17 05:37:38 +0000

    Some simple testcases

--- paludis/is_file_with_extension_TEST.cc
+++ paludis/is_file_with_extension_TEST.cc
@@ -17,8 +17,42 @@ namespace test_cases
 
         void run()
         {
-            /// \todo
+            IsFileWithExtension a("foo");
+            IsFileWithExtension b("cc");
+
+            FSEntry c("teh.foo");
+            FSEntry d("is_file_with_extension_TEST.cc");
+
+            TEST_CHECK( !a(c) );
+            TEST_CHECK( !a(d) );
+            TEST_CHECK( !b(c) );
+            TEST_CHECK( b(d) );
+
         }
     } test_is_file_with_extension;
+
+    struct IsFileWithExtensionPrefixTest : TestCase
+    {
+        IsFileWithExtensionPrefixTest() : TestCase("is file with extension (with prefix)") { }
+
+        void run()
+        {
+            IsFileWithExtension a("teh","foo");
+            IsFileWithExtension b("is", "cc");
+            IsFileWithExtension c("with", "cc");
+
+            FSEntry d("teh.foo");
+            FSEntry e("is_file_with_extension_TEST.cc");
+
+            TEST_CHECK( !a(d) );
+            TEST_CHECK( !a(e) );
+            TEST_CHECK( !b(d) );
+            TEST_CHECK( b(e) );
+            TEST_CHECK( !c(e) );
+
+        }
+    } test_is_file_with_extension_prefix;
+
+
 }
 
