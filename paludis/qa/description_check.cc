48a9c67f9302694ce25fca074baa38cc2582f437
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2006-02-14 23:43:57 +0000

    Add a check that DESCRIPTION is set and a sane length. This is a good one to copy for other basic non-dependency metadata validation checks.

---
+++ paludis/qa/description_check.cc
@@ -0,0 +1,69 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2006 Ciaran McCreesh <ciaranm@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "description_check.hh"
+
+using namespace paludis;
+using namespace paludis::qa;
+
+DescriptionCheck::DescriptionCheck()
+{
+}
+
+CheckResult
+DescriptionCheck::operator() (const EbuildCheckData & e) const
+{
+    CheckResult result(stringify(e.get<ecd_name>()) + "-" + stringify(e.get<ecd_version>()),
+            identifier());
+
+    try
+    {
+        PackageDatabaseEntry ee(e.get<ecd_name>(), e.get<ecd_version>(),
+                e.get<ecd_environment>()->package_database()->favourite_repository());
+        VersionMetadata::ConstPointer metadata(
+                e.get<ecd_environment>()->package_database()->fetch_metadata(ee));
+
+        if (0 == metadata->get(vmk_description).length())
+            result << Message(qal_major, "DESCRIPTION unset or empty");
+        else if (metadata->get(vmk_description).length() < 10)
+            result << Message(qal_minor, "DESCRIPTION suspiciously short");
+        else if (metadata->get(vmk_description).length() > 80)
+            result << Message(qal_minor, "DESCRIPTION too long");
+
+    }
+    catch (const InternalError &)
+    {
+        throw;
+    }
+    catch (const Exception & e)
+    {
+        result << Message(qal_fatal, "Caught Exception '" + e.message() + "' ("
+                + e.what() + ")");
+    }
+
+    return result;
+}
+
+const std::string &
+DescriptionCheck::identifier()
+{
+    static const std::string id("description");
+    return id;
+}
+
