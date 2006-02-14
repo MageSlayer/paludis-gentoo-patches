02b4cc12f21e20d88a4efecdb3114b95916139d2
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2006-02-14 23:55:31 +0000

    Checks for HOMEPAGE

---
+++ paludis/qa/homepage_check.cc
@@ -0,0 +1,71 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2006 Ciaran McCreesh <ciaranm@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "homepage_check.hh"
+
+using namespace paludis;
+using namespace paludis::qa;
+
+HomepageCheck::HomepageCheck()
+{
+}
+
+CheckResult
+HomepageCheck::operator() (const EbuildCheckData & e) const
+{
+    CheckResult result(stringify(e.get<ecd_name>()) + "-" + stringify(e.get<ecd_version>()),
+            identifier());
+
+    try
+    {
+        PackageDatabaseEntry ee(e.get<ecd_name>(), e.get<ecd_version>(),
+                e.get<ecd_environment>()->package_database()->favourite_repository());
+        VersionMetadata::ConstPointer metadata(
+                e.get<ecd_environment>()->package_database()->fetch_metadata(ee));
+
+        std::string homepage(metadata->get(vmk_homepage));
+
+        if (homepage.empty())
+            result << Message(qal_major, "HOMEPAGE empty or unset");
+
+        else if (std::string::npos == homepage.find("http://") &&
+                std::string::npos == homepage.find("https://") &&
+                std::string::npos == homepage.find("ftp://"))
+            result << Message(qal_major, "HOMEPAGE doesn't look like a URL");
+    }
+    catch (const InternalError &)
+    {
+        throw;
+    }
+    catch (const Exception & e)
+    {
+        result << Message(qal_fatal, "Caught Exception '" + e.message() + "' ("
+                + e.what() + ")");
+    }
+
+    return result;
+}
+
+const std::string &
+HomepageCheck::identifier()
+{
+    static const std::string id("homepage");
+    return id;
+}
+
---
+++ paludis/qa/homepage_check.hh
@@ -0,0 +1,51 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2006 Ciaran McCreesh <ciaranm@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PALUDIS_GUARD_PALUDIS_QA_HOMEPAGE_CHECK_HH
+#define PALUDIS_GUARD_PALUDIS_QA_HOMEPAGE_CHECK_HH 1
+
+#include <paludis/qa/ebuild_check.hh>
+#include <string>
+
+namespace paludis
+{
+    namespace qa
+    {
+        class HomepageCheck :
+            public EbuildCheck
+        {
+            public:
+                HomepageCheck();
+
+                CheckResult operator() (const EbuildCheckData &) const;
+
+                static const std::string & identifier();
+
+                virtual std::string describe() const
+                {
+                    return "Checks that HOMEPAGE is sane";
+                }
+        };
+
+        static const EbuildCheckMaker::RegisterMaker register_homepage_check(
+                HomepageCheck::identifier(), &make_ebuild_check<HomepageCheck>);
+    }
+}
+
+#endif
