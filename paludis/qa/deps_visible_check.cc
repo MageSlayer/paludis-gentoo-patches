807fbf0fe56d8fb53855017f912efcff6518cb20
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2006-02-15 11:38:46 +0000

    Visibility QA framework. Does nothing currently.

---
+++ paludis/qa/deps_visible_check.cc
@@ -0,0 +1,140 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2006 Ciaran McCreesh <ciaranm@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "deps_visible_check.hh"
+
+#include <paludis/dep_parser.hh>
+#include <paludis/dep_atom_visitor.hh>
+#include <paludis/any_dep_atom.hh>
+#include <paludis/all_dep_atom.hh>
+#include <paludis/package_dep_atom.hh>
+#include <paludis/block_dep_atom.hh>
+#include <paludis/use_dep_atom.hh>
+
+using namespace paludis;
+using namespace paludis::qa;
+
+namespace
+{
+    struct Checker :
+        DepAtomVisitorTypes::ConstVisitor
+    {
+        CheckResult & result;
+        const std::string role;
+        const Environment * env;
+
+        Checker(CheckResult & rr, const std::string & r, const Environment * e) :
+            result(rr),
+            role(r),
+            env(e)
+        {
+        }
+
+        void visit(const PackageDepAtom * const p)
+        {
+            bool found(false);
+            PackageDatabaseEntryCollection::Pointer matches(env->package_database()->query(p));
+            for (PackageDatabaseEntryCollection::ReverseIterator m(matches->rbegin()),
+                    m_end(matches->rend()) ; m != m_end ; ++m)
+            {
+                if (env->mask_reasons(*m).any())
+                    continue;
+
+                found = true;
+                break;
+            }
+
+            if (! found)
+                result << Message(qal_major, "No visible provider for " + role + " entry '"
+                        + stringify(*p) + "'");
+        }
+
+        void visit(const AllDepAtom * const a)
+        {
+            std::for_each(a->begin(), a->end(), accept_visitor(this));
+        }
+
+        void visit(const AnyDepAtom * const)
+        {
+        }
+
+        void visit(const UseDepAtom * const)
+        {
+        }
+
+        void visit(const BlockDepAtom * const)
+        {
+        }
+    };
+}
+
+DepsVisibleCheck::DepsVisibleCheck()
+{
+}
+
+CheckResult
+DepsVisibleCheck::operator() (const EbuildCheckData & e) const
+{
+    CheckResult result(stringify(e.get<ecd_name>()) + "-" + stringify(e.get<ecd_version>()),
+            identifier());
+
+    try
+    {
+        PackageDatabaseEntry ee(e.get<ecd_name>(), e.get<ecd_version>(),
+                e.get<ecd_environment>()->package_database()->favourite_repository());
+        VersionMetadata::ConstPointer metadata(
+                e.get<ecd_environment>()->package_database()->fetch_metadata(ee));
+
+        if (e.get<ecd_environment>()->mask_reasons(ee).any())
+            result << Message(qal_skip, "Masked, so skipping checks");
+        else
+        {
+            Checker depend_checker(result, "DEPEND", e.get<ecd_environment>());
+            std::string depend(metadata->get(vmk_depend));
+            DepParser::parse(depend)->accept(&depend_checker);
+
+            Checker rdepend_checker(result, "RDEPEND", e.get<ecd_environment>());
+            std::string rdepend(metadata->get(vmk_rdepend));
+            DepParser::parse(rdepend)->accept(&rdepend_checker);
+
+            Checker pdepend_checker(result, "PDEPEND", e.get<ecd_environment>());
+            std::string pdepend(metadata->get(vmk_pdepend));
+            DepParser::parse(pdepend)->accept(&pdepend_checker);
+        }
+    }
+    catch (const InternalError &)
+    {
+        throw;
+    }
+    catch (const Exception & e)
+    {
+        result << Message(qal_fatal, "Caught Exception '" + e.message() + "' ("
+                + e.what() + ")");
+    }
+
+    return result;
+}
+
+const std::string &
+DepsVisibleCheck::identifier()
+{
+    static const std::string id("deps visible");
+    return id;
+}
+
---
+++ paludis/qa/deps_visible_check.hh
@@ -0,0 +1,51 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2006 Ciaran McCreesh <ciaranm@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PALUDIS_GUARD_PALUDIS_QA_DEPS_VISIBLE_CHECK_HH
+#define PALUDIS_GUARD_PALUDIS_QA_DEPS_VISIBLE_CHECK_HH 1
+
+#include <paludis/qa/ebuild_check.hh>
+#include <string>
+
+namespace paludis
+{
+    namespace qa
+    {
+        class DepsVisibleCheck :
+            public EbuildCheck
+        {
+            public:
+                DepsVisibleCheck();
+
+                CheckResult operator() (const EbuildCheckData &) const;
+
+                static const std::string & identifier();
+
+                virtual std::string describe() const
+                {
+                    return "Checks that packages in *DEPEND are visible";
+                }
+        };
+
+        static const EbuildCheckMaker::RegisterMaker register_deps_visible_check(
+                DepsVisibleCheck::identifier(), &make_ebuild_check<DepsVisibleCheck>);
+    }
+}
+
+#endif
