5480757f20817cf5cbebed4c456645a7f8aa48d6
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2006-02-15 03:01:00 +0000

    Some crude checks for use? flags in *DEPEND. Currently gives various false matches.

---
+++ paludis/qa/dep_flags_check.cc
@@ -0,0 +1,136 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2006 Ciaran McCreesh <ciaranm@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "dep_flags_check.hh"
+#include <paludis/dep_parser.hh>
+#include <paludis/dep_atom_visitor.hh>
+#include <paludis/any_dep_atom.hh>
+#include <paludis/all_dep_atom.hh>
+#include <paludis/package_dep_atom.hh>
+#include <paludis/block_dep_atom.hh>
+#include <paludis/use_dep_atom.hh>
+
+using namespace paludis;
+using namespace paludis::qa;
+
+namespace
+{
+    struct Checker :
+        DepAtomVisitorTypes::ConstVisitor
+    {
+        CheckResult & result;
+        const std::string role;
+        const std::set<UseFlagName> & iuse;
+
+        Checker(CheckResult & rr, const std::string & r, const std::set<UseFlagName> & i) :
+            result(rr),
+            role(r),
+            iuse(i)
+        {
+        }
+
+        void visit(const PackageDepAtom * const)
+        {
+        }
+
+        void visit(const AllDepAtom * const a)
+        {
+            std::for_each(a->begin(), a->end(), accept_visitor(this));
+        }
+
+        void visit(const AnyDepAtom * const a)
+        {
+            std::for_each(a->begin(), a->end(), accept_visitor(this));
+        }
+
+        void visit(const UseDepAtom * const u)
+        {
+            /// \bug VV arch flags should be qal_maybe and a different message
+            if (iuse.end() == iuse.find(u->flag()))
+                result << Message(qal_major, "Conditional flag '" + stringify(u->flag()) +
+                        "' in " + role + " not in IUSE");
+
+            std::for_each(u->begin(), u->end(), accept_visitor(this));
+        }
+
+        void visit(const BlockDepAtom * const)
+        {
+        }
+    };
+}
+
+DepFlagsCheck::DepFlagsCheck()
+{
+}
+
+CheckResult
+DepFlagsCheck::operator() (const EbuildCheckData & e) const
+{
+    CheckResult result(stringify(e.get<ecd_name>()) + "-" + stringify(e.get<ecd_version>()),
+            identifier());
+
+    try
+    {
+        PackageDatabaseEntry ee(e.get<ecd_name>(), e.get<ecd_version>(),
+                e.get<ecd_environment>()->package_database()->favourite_repository());
+        VersionMetadata::ConstPointer metadata(
+                e.get<ecd_environment>()->package_database()->fetch_metadata(ee));
+
+        std::set<UseFlagName> iuse(metadata->begin_iuse(), metadata->end_iuse());
+        /// \todo VV need a more elegant way of doing this
+        iuse.insert(UseFlagName("bootstrap"));
+        iuse.insert(UseFlagName("build"));
+        iuse.insert(UseFlagName("elibc_glibc"));
+        iuse.insert(UseFlagName("userland_Darwin"));
+        iuse.insert(UseFlagName("userland_bsd"));
+        iuse.insert(UseFlagName("userland_GNU"));
+
+        Checker depend_checker(result, "DEPEND", iuse);
+        std::string depend(metadata->get(vmk_depend));
+        DepParser::parse(depend)->accept(&depend_checker);
+
+        Checker rdepend_checker(result, "RDEPEND", iuse);
+        std::string rdepend(metadata->get(vmk_rdepend));
+        DepParser::parse(rdepend)->accept(&rdepend_checker);
+
+        Checker pdepend_checker(result, "PDEPEND", iuse);
+        std::string pdepend(metadata->get(vmk_pdepend));
+        DepParser::parse(pdepend)->accept(&pdepend_checker);
+    }
+    catch (const InternalError &)
+    {
+        throw;
+    }
+    catch (const Exception & e)
+    {
+        result << Message(qal_fatal, "Caught Exception '" + e.message() + "' ("
+                + e.what() + ")");
+    }
+
+    return result;
+}
+
+const std::string &
+DepFlagsCheck::identifier()
+{
+    static const std::string id("dep flags");
+    return id;
+}
+
+
