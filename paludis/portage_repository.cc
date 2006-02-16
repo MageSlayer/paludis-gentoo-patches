5d7a0acfa9eff49cd39a7d5a03f35bce0a810b79
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2006-02-16 17:43:42 +0000

    Drop the repo_name message to qa

--- paludis/portage_repository.cc
+++ paludis/portage_repository.cc
@@ -505,7 +505,7 @@ PortageRepository::fetch_repo_name(const std::string & location)
     catch (...)
     {
     }
-    Log::get_instance()->message(ll_warning, "Couldn't open repo_name file in '"
+    Log::get_instance()->message(ll_qa, "Couldn't open repo_name file in '"
             + location + "/profiles/'. Falling back to a generated name.");
     return RepositoryName("x-" + location);
 }
