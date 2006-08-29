598bc976ba583cb66c5330088ef1fe6e62f853b1
Mike Kelly <pioto@pioto.org>
2006-08-29 17:44:01 +0000

    Add copyright header.

--- hooks/update_config_protect_list.bash
+++ hooks/update_config_protect_list.bash
@@ -1,6 +1,22 @@
 #!/bin/bash
 # vim: set et sw=4 sts=4 :
 
+# Copyright (c) 2006 Mike Kelly <pioto@pioto.org>
+#
+# This file is part of the Paludis package manager. Paludis is free software;
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation; either version
+# 2 of the License, or (at your option) any later version.
+#
+# Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
 vdb_loc=$(${PALUDIS_COMMAND} --configuration-variable installed location )
 cfg_protect_list="${vdb_loc}/.cache/all_CONFIG_PROTECT"
 cfg_protect_mask_list="${vdb_loc}/.cache/all_CONFIG_PROTECT_MASK"
