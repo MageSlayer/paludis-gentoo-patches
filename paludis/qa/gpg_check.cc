cfbc5f8f04f6131e6f9b148b8706961b28390a14
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2006-11-04 01:39:40 +0000

    Distinguish between bad, unsigned and unknown signatures

--- paludis/qa/gpg_check.cc
+++ paludis/qa/gpg_check.cc
@@ -20,12 +20,12 @@
 #include <paludis/qa/gpg_check.hh>
 #include <paludis/util/system.hh>
 #include <paludis/util/fd_holder.hh>
-
-#include <iostream>
+#include <fstream>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <pcre++.h>
 
 using namespace paludis;
 using namespace paludis::qa;
@@ -38,17 +38,44 @@ CheckResult
 GPGCheck::operator() (const FSEntry & d) const
 {
     CheckResult result(d, identifier());
-    FSEntry manifest(d / "Manifest");
-    FDHolder dev_null(::open("/dev/null", O_WRONLY));
 
-    set_run_command_stdout_fds(dev_null, -1);
-    set_run_command_stderr_fds(dev_null, -1);
+    if (! (d / "Manifest").is_regular_file())
+    {
+        result << Message(qal_major, "No Manifest");
+        return result;
+    }
 
-    int status(run_command("gpg --verify " + stringify(manifest)));
+    static pcrepp::Pcre::Pcre r_is_signed("^-----BEGIN PGP SIGNED MESSAGE-----");
+    bool is_signed(false);
+    {
+        std::ifstream ff(stringify(d / "Manifest").c_str());
+        if (! ff)
+            result << Message(qal_major, "Can't read file");
+        else
+        {
+            std::string s;
+            while ((! is_signed) && std::getline(ff, s))
+                if (r_is_signed.search(s))
+                    is_signed = true;
+        }
+    }
 
-    if (1 == status)
-        result << Message(qal_major, "Broken Manifest signature");
-    else if (2 == status)
+    if (is_signed)
+    {
+        FSEntry manifest(d / "Manifest");
+        FDHolder dev_null(::open("/dev/null", O_WRONLY));
+
+        set_run_command_stdout_fds(dev_null, -1);
+        set_run_command_stderr_fds(dev_null, -1);
+
+        int status(run_command("gpg --verify " + stringify(manifest)));
+
+        if (1 == status)
+            result << Message(qal_major, "Broken Manifest signature");
+        else if (2 == status)
+            result << Message(qal_maybe, "Manifest signature cannot be verified");
+    }
+    else
         result << Message(qal_minor, "Manifest not signed");
 
     return result;
