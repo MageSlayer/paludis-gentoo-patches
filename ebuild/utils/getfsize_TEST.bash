95c0a8dc6a3905117fcefde8f33192482fe2523d
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2007-01-05 01:00:44 +0000

    Add tests for getfsize

---
+++ ebuild/utils/getfsize_TEST.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+# vim: set sw=4 sts=4 et :
+
+# Copyright (c) 2007 Ciaran McCreesh <ciaranm@ciaranm.org>
+#
+# This file is part of the Paludis package manager. Paludis is free software;
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License version 2, as published by the Free Software Foundation.
+#
+# Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
+getfsize_TEST()
+{
+    mkdir -p getfsize_TEST_dir ; test_return_code
+
+    echo -n "12345" > getfsize_TEST_dir/five
+    echo "12345" > getfsize_TEST_dir/six
+
+    ${PALUDIS_EBUILD_DIR}/utils/getfsize getfsize_TEST_dir/five >/dev/null ; test_return_code
+    ${PALUDIS_EBUILD_DIR}/utils/getfsize getfsize_TEST_dir/six >/dev/null ; test_return_code
+
+    [[ $(${PALUDIS_EBUILD_DIR}/utils/getfsize getfsize_TEST_dir/five ) == "5" ]] ; test_return_code
+    [[ $(${PALUDIS_EBUILD_DIR}/utils/getfsize getfsize_TEST_dir/six ) == "6" ]] ; test_return_code
+
+    rm -fr getfsize_TEST_dir
+}
+
+getfsize_fail_TEST()
+{
+    mkdir -p getfsize_TEST_dir ; test_return_code
+
+    ! ${PALUDIS_EBUILD_DIR}/utils/getfsize getfsize_TEST_dir/seven 2>/dev/null ; test_return_code
+
+    rm -fr getfsize_TEST_dir
+}
+
+
