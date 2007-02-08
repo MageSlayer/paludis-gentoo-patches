c5787e23d6f52df06d166079e23c4cf231c3de25
Richard Brown <rbrown@exherbo.org>
2007-02-08 10:57:18 +0000

    Remove QA warning for exporting pkg_nofetch and pkg_config.

--- ebuild/eclass_functions.bash
+++ ebuild/eclass_functions.bash
@@ -27,7 +27,7 @@ EXPORT_FUNCTIONS()
     local e
     for e in "$@" ; do
         case "$e" in
-            pkg_setup|pkg_prerm|pkg_postrm|pkg_preinst|pkg_postinst)
+            pkg_nofetch|pkg_setup|pkg_prerm|pkg_postrm|pkg_preinst|pkg_postinst|pkg_config)
                 eval "${e}() { ${ECLASS}_${e} \"\$@\" ; }"
                 ;;
 
