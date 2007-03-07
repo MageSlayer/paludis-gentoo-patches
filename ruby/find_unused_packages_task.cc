18f8ce9abc51d4d1b4be0240b81a5d840709c1e3
Richard Brown <rbrown@exherbo.org>
2007-03-07 22:44:15 +0000

    Add FindUnusedPackageTask to ruby.

---
+++ ruby/find_unused_packages_task.cc
@@ -0,0 +1,101 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 Richard Brown <rbrown@gentoo.org>
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <paludis_ruby.hh>
+#include <paludis/tasks/find_unused_packages_task.hh>
+#include <ruby.h>
+
+using namespace paludis;
+using namespace paludis::ruby;
+
+#define RUBY_FUNC_CAST(x) reinterpret_cast<VALUE (*)(...)>(x)
+
+namespace
+{
+    static VALUE c_find_unused_packages_task;
+
+    VALUE
+    find_unused_packages_task_init(int, VALUE *, VALUE self)
+    {
+        return self;
+    }
+
+    VALUE
+    find_unused_packages_task_new(int argc, VALUE *argv, VALUE self)
+    {
+        FindUnusedPackagesTask * ptr(0);
+        try
+        {
+            if (2 == argc)
+            {
+                ptr = new FindUnusedPackagesTask(value_to_environment_data(argv[0])->env_ptr,
+                    value_to_repository(argv[1]).get());
+            }
+            else
+            {
+                rb_raise(rb_eArgError, "FindUnusedPackagesTask expects two arguments, but got %d",argc);
+            }
+            VALUE tdata(Data_Wrap_Struct(self, 0, &Common<FindUnusedPackagesTask>::free, ptr));
+            rb_obj_call_init(tdata, argc, argv);
+            return tdata;
+        }
+        catch (const std::exception & e)
+        {
+            delete ptr;
+            exception_to_ruby_exception(e);
+        }
+    }
+
+    VALUE find_unused_packages_task_execute(VALUE self, VALUE qpn)
+    {
+        try
+        {
+            FindUnusedPackagesTask * ptr;
+            Data_Get_Struct(self, FindUnusedPackagesTask, ptr);
+            std::tr1::shared_ptr<const PackageDatabaseEntryCollection> c(ptr->execute(value_to_qualified_package_name(qpn)));
+            VALUE result(rb_ary_new());
+            for (PackageDatabaseEntryCollection::Iterator i(c->begin()), i_end(c->end()) ; i != i_end ; ++i)
+                rb_ary_push(result, package_database_entry_to_value(*i));
+            return result;
+        }
+        catch (const std::exception & e)
+        {
+            exception_to_ruby_exception(e);
+        }
+    }
+
+    void do_register_find_unused_packages_task()
+    {
+        /* Document-class: Paludis::QA::Message
+         *
+         * A QA message
+         */
+        c_find_unused_packages_task = rb_define_class_under(paludis_module(),
+                "FindUnusedPackagesTask", rb_cObject);
+        rb_define_singleton_method(c_find_unused_packages_task,
+                "new", RUBY_FUNC_CAST(&find_unused_packages_task_new),-1);
+        rb_define_method(c_find_unused_packages_task,
+                "initialize", RUBY_FUNC_CAST(&find_unused_packages_task_init),-1);
+        rb_define_method(c_find_unused_packages_task,
+                "execute", RUBY_FUNC_CAST(&find_unused_packages_task_execute),1);
+    }
+}
+
+RegisterRubyClass::Register paludis_ruby_register_find_unused_packages_task PALUDIS_ATTRIBUTE((used))
+    (&do_register_find_unused_packages_task);
---
+++ ruby/find_unused_packages_task_TEST_cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim: set ft=sh sw=4 sts=4 et :
+
+if [ -d find_unused_packages_task_TEST_dir ] ; then
+    rm -fr find_unused_packages_task_TEST_dir
+else
+    true
+fi
+
+
+
+
