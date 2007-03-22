752bdb2cfb974b8b88305025a44c42177c3af4f3
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2007-03-22 16:44:03 +0000

    Typo

--- paludis/name.hh
+++ paludis/name.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2005, 2006 Ciaran McCreesh <ciaranm@ciaranm.org>
+ * Copyright (c) 2005, 2006, 2007 Ciaran McCreesh <ciaranm@ciaranm.org>
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -71,7 +71,7 @@ namespace paludis
 
     /**
      * A PackageNamePart holds a std::string that is a valid name for the
-     * category part of a QualifiedPackageName.
+     * package part of a QualifiedPackageName.
      *
      * \ingroup grpnames
      */
