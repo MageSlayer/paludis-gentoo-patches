2cf1c2626c874a6d5ae2c4a4a0c9e8821fe2a636
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2007-04-16 02:49:17 +0000

    Mark string constructor as explicit

--- paludis/version_operator.hh
+++ paludis/version_operator.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2005, 2006 Ciaran McCreesh <ciaranm@ciaranm.org>
+ * Copyright (c) 2005, 2006, 2007 Ciaran McCreesh <ciaranm@ciaranm.org>
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -92,7 +92,7 @@ namespace paludis
             /**
              * Constructor, from a string.
              */
-            VersionOperator(const std::string & v) :
+            explicit VersionOperator(const std::string & v) :
                 ComparisonPolicyType(&VersionOperator::_v),
                 _v(_decode(v))
             {
