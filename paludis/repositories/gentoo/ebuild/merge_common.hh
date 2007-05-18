4b4fb91fb504202f523ad6532a2073385354564a
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2007-05-18 23:16:17 +0000

    Fix g++-4.3 compile

--- paludis/repositories/gentoo/ebuild/merge_common.hh
+++ paludis/repositories/gentoo/ebuild/merge_common.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2006 Ciaran McCreesh <ciaranm@ciaranm.org>
+ * Copyright (c) 2006, 2007 Ciaran McCreesh <ciaranm@ciaranm.org>
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -37,7 +37,7 @@ namespace merge
         Failure(const std::string & m) :
             message(m)
         {
-        };
+        }
     };
 
     std::vector<std::string>
