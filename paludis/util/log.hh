66cc8b93a7d3c649be8b2a5a8d2277a9fe5dc2a7
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2007-06-29 00:46:40 +0000

    r3660@snowflake:  ciaranm | 2007-06-29 01:46:22 +0100  Default log level of ll_qa, not ll_debug

--- paludis/util/log.hh
+++ paludis/util/log.hh
@@ -52,7 +52,7 @@ namespace paludis
         ll_warning,           ///< Warning message
         ll_silent,            ///< Silent (for set_log_level)
         last_ll,              ///< Number of items
-        initial_ll = ll_debug ///< Initial value
+        initial_ll = ll_qa    ///< Initial value
     };
 
 #include <paludis/util/log-se.hh>
