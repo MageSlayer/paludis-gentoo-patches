f2f714eac35c1f51690d7f9ac3e98ce2bc35c587
Alexander Færøy <eroyf@eroyf.org>
2007-08-13 17:02:21 +0000

    Fix email.

--- ruby/query.cc
+++ ruby/query.cc
@@ -2,7 +2,7 @@
 
 /*
  * Copyright (c) 2007 Richard Brown <rbrown@gentoo.org>
- * Copyright (c) 2007 Alexander Færøy <eroyf@gentoo.org>
+ * Copyright (c) 2007 Alexander Færøy <eroyf@eroyf.org>
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
