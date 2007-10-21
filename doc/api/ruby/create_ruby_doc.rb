ec08464a55494151c4dfb9ca6f9e5a7f1a99d3e7
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2007-10-21 17:54:52 +0000

    Fix out of tree build

--- doc/api/ruby/create_ruby_doc.rb
+++ doc/api/ruby/create_ruby_doc.rb
@@ -55,7 +55,7 @@ module RDoc
 
         def generate_consts(header, type, in_class)
             consts = []
-            file = File.read("../../../#{header}")
+            file = File.read(ENV["TOP_SRCDIR"] + "/#{header}")
             match = Regexp.new(/enum\s+#{type}\s+\{([^}]+)\}/)#, Regexp::MULTILINE)
             if file =~ match
                 enum = $1
