902d089ea4bcf9658e282fd7427462d8e821bb99
Richard Brown <rbrown@exherbo.org>
2007-11-18 16:49:01 +0000

    (ruby) Add example_environment.

---
+++ doc/api/ruby/example_environment.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# vim: set sw=4 sts=4 et tw=80 :
+
+=begin description
+This example demonstrates how to use EnvironmentMaker and the resultant
+Environment.
+=end
+
+require 'Paludis'
+require 'example_command_line'
+
+include Paludis
+
+exit_status = 0
+
+# We use EnvironmentMaker to construct an environment from the user's
+# --environment commandline choice. With an empty string, this uses the
+# distribution-defined default environment. With a non-empty string, it
+# is split into two parts upon the first colon (if there is no colon,
+# the second part is considered empty). The first part is the name of
+# the environment class to use (e.g. 'paludis', 'portage') and the
+# second part is passed as parameters to be handled by that
+# environment's constructor.
+env = EnvironmentMaker.instance.make_from_spec(ExampleCommandLine.instance.environment)
+
+# A lot of the Environment members aren't very useful to clients. The
+# mask related methods are used by PackageID, and shouldn't usually be
+# called directly from clients. The system information and mirror
+# functions are mostly for use by Repository subclasses. That leaves
+# the package database, sets and (currently, although this may well
+# change in the future) use flag queries. The package database has its
+# own examples, so we'll start with sets:
+
+world = env.set('world')
+if (world)
+
+    # see examples_dep_tree.rb for how to make use of this set
+    puts "World set exists"
+else
+    puts "No world set defined"
+end
+
+# And use flags, for which we need package IDs:
+ids = env.package_database.query(
+    Query::Matches.new(PackageDepSpec.new('sys-apps/paludis', PackageDepSpecParseMode::Permissive)) &
+    Query::SupportsInstalledAction.new,
+    QueryOrder::OrderByVersion)
+
+if (ids.length > 0)
+    id = ids.last
+    print "Use flag 'ruby' for ID '#{id.to_s}' is "
+    puts env.query_use('ruby', id) ? 'enabled' : 'disabled'
+end
