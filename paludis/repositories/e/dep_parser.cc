fecbdf3fca996af957777519b143505b92442407
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2008-01-01 23:27:41 +0000

    Add in post label parsing

--- paludis/repositories/e/dep_parser.cc
+++ paludis/repositories/e/dep_parser.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2005, 2006, 2007 Ciaran McCreesh
+ * Copyright (c) 2005, 2006, 2007, 2008 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -796,6 +796,8 @@ paludis::erepository::parse_dependency_label(const std::string & s, const EAPI &
             l->add_label(make_shared_ptr(new DependencyBuildLabel(*it)));
         else if (c == "DependencyRunLabel")
             l->add_label(make_shared_ptr(new DependencyRunLabel(*it)));
+        else if (c == "DependencyPostLabel")
+            l->add_label(make_shared_ptr(new DependencyPostLabel(*it)));
         else if (c == "DependencyInstallLabel")
             l->add_label(make_shared_ptr(new DependencyInstallLabel(*it)));
         else if (c == "DependencyCompileLabel")
