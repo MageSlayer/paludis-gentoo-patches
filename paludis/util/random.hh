6f64586efde03d944d81d028093e165d3a2408fb
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2008-02-29 01:15:40 +0000

    std::Multiplicable is now std::HasMultiply

--- paludis/util/random.hh
+++ paludis/util/random.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2006, 2007 Ciaran McCreesh
+ * Copyright (c) 2006, 2007, 2008 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -76,7 +76,7 @@ namespace paludis
             template <typename DiffType_>
 #ifdef PALUDIS_HAVE_CONCEPTS
                 requires
-                    std::Multiplicable<double, DiffType_>,
+                    std::HasMultiply<double, DiffType_>,
                     std::CopyConstructible<DiffType_>
 #endif
             DiffType_ operator() (DiffType_ max)
