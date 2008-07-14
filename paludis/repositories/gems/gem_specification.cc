bb9aecd155d593f318aa0ecf0f0fc484e5fe7e99
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2008-07-14 03:01:13 +0100

    Update Gems for previous (continued)

--- paludis/repositories/gems/gem_specification.cc
+++ paludis/repositories/gems/gem_specification.cc
@@ -420,16 +420,10 @@ GemSpecification::installed_time_key() const
     return std::tr1::shared_ptr<const MetadataTimeKey>();
 }
 
-const std::tr1::shared_ptr<const MetadataValueKey<std::string> >
-GemSpecification::source_origin_key() const
+const std::tr1::shared_ptr<const MetadataCollectionKey<Set<std::string> > >
+GemSpecification::from_repositories_key() const
 {
-    return std::tr1::shared_ptr<const MetadataValueKey<std::string> >();
-}
-
-const std::tr1::shared_ptr<const MetadataValueKey<std::string> >
-GemSpecification::binary_origin_key() const
-{
-    return std::tr1::shared_ptr<const MetadataValueKey<std::string> >();
+    return std::tr1::shared_ptr<const MetadataCollectionKey<Set<std::string> > >();
 }
 
 const std::tr1::shared_ptr<const MetadataCollectionKey<PackageIDSequence> >
--- paludis/repositories/gems/gem_specification.hh
+++ paludis/repositories/gems/gem_specification.hh
@@ -108,8 +108,7 @@ namespace paludis
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<FSEntry> > fs_location_key() const;
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<std::tr1::shared_ptr<const Contents> > > contents_key() const;
                 virtual const std::tr1::shared_ptr<const MetadataTimeKey> installed_time_key() const;
-                virtual const std::tr1::shared_ptr<const MetadataValueKey<std::string> > source_origin_key() const;
-                virtual const std::tr1::shared_ptr<const MetadataValueKey<std::string> > binary_origin_key() const;
+                virtual const std::tr1::shared_ptr<const MetadataCollectionKey<Set<std::string> > > from_repositories_key() const;
 
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<bool> > transient_key() const;
 
