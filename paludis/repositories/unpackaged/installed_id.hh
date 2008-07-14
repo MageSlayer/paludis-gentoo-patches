3b627dbc6471f30a359cad3817bc99090ca8a7ca
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2008-07-14 03:03:20 +0100

    Update Unpackaged for previous (continued)

--- paludis/repositories/unpackaged/installed_id.hh
+++ paludis/repositories/unpackaged/installed_id.hh
@@ -73,10 +73,9 @@ namespace paludis
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<std::string> > long_description_key() const;
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<std::tr1::shared_ptr<const Contents> > > contents_key() const;
                 virtual const std::tr1::shared_ptr<const MetadataTimeKey> installed_time_key() const;
-                virtual const std::tr1::shared_ptr<const MetadataValueKey<std::string> > source_origin_key() const;
-                virtual const std::tr1::shared_ptr<const MetadataValueKey<std::string> > binary_origin_key() const;
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<FSEntry> > fs_location_key() const;
                 virtual const std::tr1::shared_ptr<const MetadataValueKey<bool> > transient_key() const;
+                virtual const std::tr1::shared_ptr<const MetadataCollectionKey<Set<std::string> > > from_repositories_key() const;
 
                 virtual bool supports_action(const SupportsActionTestBase &) const PALUDIS_ATTRIBUTE((warn_unused_result));
                 virtual void perform_action(Action &) const;
