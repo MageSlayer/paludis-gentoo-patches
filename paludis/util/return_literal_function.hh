4782eea70f1dcfabb3187f465f3f2a2ea290591a
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2009-10-11 09:05:00 +0100

    helper for a function returning a literal

---
+++ paludis/util/return_literal_function.hh
@@ -0,0 +1,50 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2009 Ciaran McCreesh
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PALUDIS_GUARD_PALUDIS_UTIL_RETURN_LITERAL_FUNCTION_HH
+#define PALUDIS_GUARD_PALUDIS_UTIL_RETURN_LITERAL_FUNCTION_HH 1
+
+#include <paludis/util/return_literal_function-fwd.hh>
+
+namespace paludis
+{
+    template <typename T_>
+    struct ReturnLiteralFunction
+    {
+        const T_ value;
+
+        const T_ operator() () const PALUDIS_ATTRIBUTE((warn_unused_result))
+        {
+            return value;
+        }
+
+        ReturnLiteralFunction(const T_ & v) :
+            value(v)
+        {
+        }
+    };
+
+    template <typename T_>
+    std::tr1::function<T_ ()> return_literal_function(const T_ & v)
+    {
+        return ReturnLiteralFunction<T_>(v);
+    }
+}
+
+#endif
