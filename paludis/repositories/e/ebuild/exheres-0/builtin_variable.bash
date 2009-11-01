c681ba43865c92dd7bc2f08908dc077c28d49504
David Leverton <levertond@googlemail.com>
2009-11-01 22:53:34 +0000

    cfi

--- paludis/repositories/e/ebuild/exheres-0/builtin_variable.bash
+++ paludis/repositories/e/ebuild/exheres-0/builtin_variable.bash
@@ -16,7 +16,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 
-ebuild_f_variable()
+exheres_internal_variable()
 {
     echo "${!PALUDIS_VARIABLE}"
 }
