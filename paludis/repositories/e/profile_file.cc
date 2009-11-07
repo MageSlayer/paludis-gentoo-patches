e62938f063c1e953bddfdf055dddbe1ab5a9a035
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2009-11-07 14:55:53 +0000

    fix

--- paludis/repositories/e/profile_file.cc
+++ paludis/repositories/e/profile_file.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2006, 2007, 2008 Ciaran McCreesh
+ * Copyright (c) 2006, 2007, 2008, 2009 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -17,7 +17,7 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <paludis/repositories/e/e_repository_profile_file.hh>
+#include <paludis/repositories/e/profile_file.hh>
 #include <paludis/repositories/e/e_repository_mask_file.hh>
 #include <paludis/repositories/e/e_repository_exceptions.hh>
 #include <paludis/repositories/e/eapi.hh>
