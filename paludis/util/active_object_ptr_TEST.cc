d72c9d13daefb6e329b2d80faca940f77912b2fd
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2009-11-13 15:42:02 +0000

    Make non-dereferencing use cleaner

--- paludis/util/active_object_ptr_TEST.cc
+++ paludis/util/active_object_ptr_TEST.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2008 Ciaran McCreesh
+ * Copyright (c) 2008, 2009 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -47,5 +47,20 @@ namespace test_cases
             TEST_CHECK_EQUAL(q->length(), 5u);
         }
     } test_dereference;
+
+    struct TestValue : TestCase
+    {
+        TestValue() : TestCase("value") { }
+
+        void run()
+        {
+            ActiveObjectPtr<std::string *> p(new std::string("monkey"));
+            TEST_CHECK_EQUAL(p.value()->length(), 6u);
+
+            ActiveObjectPtr<std::tr1::shared_ptr<std::string> > q(
+                    make_shared_ptr(new std::string("chimp")));
+            TEST_CHECK_EQUAL(q.value()->length(), 5u);
+        }
+    } test_value;
 }
 
--- paludis/util/deferred_construction_ptr.hh
+++ paludis/util/deferred_construction_ptr.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2008 Ciaran McCreesh
+ * Copyright (c) 2008, 2009 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -69,6 +69,11 @@ namespace paludis
                 }
 
                 return _ptr;
+            }
+
+            T_ value() const
+            {
+                return operator-> ();
             }
     };
 }
