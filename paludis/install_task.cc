80d9890571ce00e2374ab7320ee037ad893dde74
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2009-12-26 15:41:10 +0000

    typso

--- paludis/install_task.cc
+++ paludis/install_task.cc
@@ -1394,7 +1394,7 @@ InstallTask::world_update_set(const SetName & s)
 {
     if (s == SetName("world") || s == SetName("system") || s == SetName("security")
             || s == SetName("everything") || s == SetName("installed-packages")
-            || s == SetName("installed-slotted") || s == SetName("insecurity"))
+            || s == SetName("installed-slots") || s == SetName("insecurity"))
     {
         on_update_world_skip(s, "special sets cannot be added to world");
         return;
