88898b629cd93472cf249f601190466c104e99a5
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2010-01-03 20:15:23 +0000

    Get columns right when newlines are present

--- src/clients/cave/format_string.cc
+++ src/clients/cave/format_string.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2008, 2009 Ciaran McCreesh
+ * Copyright (c) 2008, 2009, 2010 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -102,6 +102,8 @@ paludis::cave::format_string(
                                     if ('m' == result[q])
                                         break;
                             }
+                            else if ('\n' == result[q])
+                                l = 0;
                             else
                                 ++l;
 
