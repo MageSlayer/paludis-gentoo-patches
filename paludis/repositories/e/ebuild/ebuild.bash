650d3cec1dfef63ed5a0e32820e5ac0e79cfb4b2
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2010-01-07 15:48:00 +0000

    Curb various interactivity abuses

--- paludis/repositories/e/ebuild/ebuild.bash
+++ paludis/repositories/e/ebuild/ebuild.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: set sw=4 sts=4 et :
 
-# Copyright (c) 2006, 2007, 2008, 2009 Ciaran McCreesh
+# Copyright (c) 2006, 2007, 2008, 2009, 2010 Ciaran McCreesh
 #
 # Based in part upon ebuild.sh from Portage, which is Copyright 1995-2005
 # Gentoo Foundation and distributed under the terms of the GNU General
@@ -49,6 +49,10 @@ ebuild_sanitise_envvars()
     unset GZIP BZIP BZIP2 CDPATH GREP_OPTIONS GREP_COLOR GLOBIGNORE
     unset LANG ${!LC_*}
     export LC_ALL=C
+
+    # pagers won't run with redirected output, but some naughty packages like
+    # to use them.
+    export PAGER=cat
 }
 ebuild_sanitise_envvars
 
