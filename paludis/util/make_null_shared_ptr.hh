41b3c32bf2fd6d51d00f7152847dead978f7faf6
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2010-07-23 12:26:02 +0100

    Fix

--- paludis/util/make_null_shared_ptr.hh
+++ paludis/util/make_null_shared_ptr.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2010 Ciaran McCreesh
+ * Copyright (c) 2007, 2008, 2010 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -19,28 +19,6 @@
 
 #ifndef PALUDIS_GUARD_PALUDIS_UTIL_MAKE_NULL_SHARED_PTR_HH
 #define PALUDIS_GUARD_PALUDIS_UTIL_MAKE_NULL_SHARED_PTR_HH 1
-
-/* vim: set sw=4 sts=4 et foldmethod=syntax : */
-
-/*
- * Copyright (c) 2007, 2008 Ciaran McCreesh
- *
- * This file is part of the Paludis package manager. Paludis is free software;
- * you can redistribute it and/or modify it under the terms of the GNU General
- * Public License version 2, as published by the Free Software Foundation.
- *
- * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-#ifndef PALUDIS_GUARD_PALUDIS_UTIL_MAKE_SHARED_PTR_HH
-#define PALUDIS_GUARD_PALUDIS_UTIL_MAKE_SHARED_PTR_HH 1
 
 #include <memory>
 
@@ -78,7 +56,5 @@ namespace paludis
         return NullSharedPtr();
     }
 }
-
-#endif
 
 #endif
