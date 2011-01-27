d8bbe9f02de950130b39d4824e5e90a870d5b302
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2011-01-27 14:06:54 +0000

    Add note to update-world man page

--- src/clients/cave/cmd_update_world.cc
+++ src/clients/cave/cmd_update_world.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2009, 2010 Ciaran McCreesh
+ * Copyright (c) 2009, 2010, 2011 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -61,7 +61,8 @@ namespace
 
         virtual std::string app_description() const
         {
-            return "Adds or removes items from the world set.";
+            return "Adds or removes items from the world set. This command is only for changing the contents "
+                "of the world set; it does not install or uninstall anything.";
         }
 
         args::ArgsGroup g_output_options;
