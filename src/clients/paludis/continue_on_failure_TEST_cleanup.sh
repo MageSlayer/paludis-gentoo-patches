95941e66453feb65895b597e305403776da96b42
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2011-02-05 22:38:55 +0000

    Kill paludis

--- src/clients/paludis/continue_on_failure_TEST_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# vim: set ft=sh sw=4 sts=4 et :
-
-if [ -d continue_on_failure_TEST_dir ] ; then
-    rm -fr continue_on_failure_TEST_dir
-else
-    true
-fi
-
--- src/clients/paludis/pretend_TEST_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# vim: set ft=sh sw=4 sts=4 et :
-
-if [ -d pretend_TEST_dir ] ; then
-    rm -fr pretend_TEST_dir
-else
-    true
-fi
-
