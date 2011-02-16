e6d1c28fbc15ae287fc5641fe1d365bddd113b9f
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2011-02-16 16:02:25 +0000

    asciidocify accerso

--- src/clients/accerso/man_accerso.cc
+++ src/clients/accerso/man_accerso.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2006, 2007, 2009 Ciaran McCreesh
+ * Copyright (c) 2006, 2007, 2009, 2011 Ciaran McCreesh
  *
  * This file is part of the Paludis package manager. Paludis is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -19,6 +19,7 @@
 
 #include "command_line.hh"
 #include <paludis/args/man.hh>
+#include <paludis/args/do_help.hh>
 
 #include <iostream>
 #include <cstdlib>
@@ -32,10 +33,12 @@ namespace
         paludis::args::ArgsHandler
     {
         paludis::args::ArgsGroup group;
+        paludis::args::SwitchArg a_asciidoc;
         paludis::args::SwitchArg a_html;
 
         ManCommandLine() :
             group(main_options_section(), "", ""),
+            a_asciidoc(&group, "asciidoc", '\0', "", false),
             a_html(&group, "html", '\0', "", false)
         {
         }
@@ -63,16 +66,15 @@ main(int argc, char * argv[])
     ManCommandLine cmdline;
     cmdline.run(argc, argv, "", "", "");
 
-    if (cmdline.a_html.specified())
-    {
-        paludis::args::HtmlWriter hw(cout);
-        paludis::args::generate_doc(hw, CommandLine::get_instance());
-    }
+    std::shared_ptr<paludis::args::DocWriter> w;
+    if (cmdline.a_asciidoc.specified())
+        w = std::make_shared<paludis::args::AsciidocWriter>(cout);
+    else if (cmdline.a_html.specified())
+        w = std::make_shared<paludis::args::HtmlWriter>(cout);
     else
-    {
-        paludis::args::ManWriter mw(cout);
-        paludis::args::generate_doc(mw, CommandLine::get_instance());
-    }
+        throw paludis::args::DoHelp("No format specified");
+
+    paludis::args::generate_doc(*w, CommandLine::get_instance());
 
     return EXIT_SUCCESS;
 }
