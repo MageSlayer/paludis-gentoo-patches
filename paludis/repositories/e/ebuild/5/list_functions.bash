bfb1bd7682b4a678cfa7dccb87d1f49d842317ac
David Leverton <levertond@googlemail.com>
2015-03-21 20:04:30 +0000

    Only check IUSE_EFFECTIVE for EAPI 5
    In other EAPIs the value isn't set by C++, and therefore may leak in
    from the calling environment.

--- paludis/repositories/e/ebuild/5/list_functions.bash
+++ paludis/repositories/e/ebuild/5/list_functions.bash
@@ -2,6 +2,7 @@
 # vim: set sw=4 sts=4 et :
 
 # Copyright (c) 2006, 2009, 2012 Ciaran McCreesh
+# Copyright (c) 2015 David Leverton
 #
 # Based in part upon ebuild.sh from Portage, which is Copyright 1995-2005
 # Gentoo Foundation and distributed under the terms of the GNU General
@@ -20,52 +21,21 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 
-use()
-{
-    useq "${1}"
-}
-
-usev()
-{
-    if useq "${1}" ; then
-        echo "${1#!}"
-        return 0
-    else
-        return 1
-    fi
-}
+ebuild_load_module --older list_functions
 
 useq()
 {
+    if [[ -n "${IUSE_EFFECTIVE:+x}" ]] ; then
+        local i=( $IUSE_EFFECTIVE )
+        if ! hasq ${1#!} "${i[@]#[+-]}" ; then
+            die "Flag '${1#!}' is not included in IUSE_EFFECTIVE=\"${IUSE_EFFECTIVE}\""
+        fi
+    fi
+
     if [[ "${1:0:1}" == "!" ]] ; then
         ! hasq "${1#!}" "${USE}"
     else
         hasq "${1}" "${USE}"
     fi
-}
-
-has()
-{
-    hasq "${@}"
-}
-
-hasv()
-{
-    if hasq "${@}" ; then
-        echo "${1}"
-        return 0
-    else
-        return 1
-    fi
-}
-
-hasq()
-{
-    local x= want="$1"
-    shift
-    for x in $@ ; do
-        [[ "${x}" == "${want}" ]] && return 0
-    done
-    return 1
 }
 
