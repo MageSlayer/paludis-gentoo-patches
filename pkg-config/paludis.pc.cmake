a38b1cff45024cebf45c6b032aa8a7ca7ea1757e
Marvin Schmidt <marv@exherbo.org>
2016-12-09 11:10:04 +0100

    pkg-config: Fix includedir and version
    Change-Id: I6d0b6e778067f306a5335b6fd3fb51a42201cddf

--- pkg-config/paludis.pc.cmake
+++ pkg-config/paludis.pc.cmake
@@ -1,11 +1,11 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
 libdir=@CMAKE_INSTALL_FULL_LIBDIR@
-includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
+includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@/paludis-@PALUDIS_PKG_CONFIG_SLOT@
 
 Name: Paludis
 Description: The Other Package Mangler
-Version: @PALUDIS_PC_SLOT@
+Version: @PALUDIS_PKG_CONFIG_SLOT@
 Libs: -L${libdir} @PALUDIS_PKG_CONFIG_LIBS@
 Cflags: -I${includedir} @PALUDIS_PKG_CONFIG_CFLAGS@
 
