6afb441957713357f74d9495414b8375f193589b
Marvin Schmidt <marv@exherbo.org>
2019-01-26 06:38:44 +0100

    ci: Add clang based build for Exherbo
    Allow the tests to fail for now, since all tests involving python fail
    for some reason when using Clang. I'll hunt this down, but like to have
    a CI job for this in place before

--- ci/docker/update-images.sh
+++ ci/docker/update-images.sh
@@ -2,6 +2,7 @@
 # vim: set sw=4 sts=4 ts=4 et tw=80 :
 
 docker build -t paludis/exherbo-gcc exherbo/paludis-exherbo-gcc
+docker build -t paludis/exherbo-clang exherbo/paludis-exherbo-clang
 
 docker build -t paludis/gentoo-gcc gentoo/paludis-gentoo-gcc
 
