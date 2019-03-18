5fa2044f91ef709ae076927bc354ce6f78905356
Marvin Schmidt <marv@exherbo.org>
2019-03-18 23:27:44 +0100

    ci: Add Exherbo testing build
    Builds paludis with GCC in an Exherbo environment with all
    testing/broken packages unmasked, so that we catch problems
    with future versions of those packages early

--- ci/docker/exherbo/paludis-exherbo-gcc/build.sh
+++ ci/docker/exherbo/paludis-exherbo-gcc/build.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 # vim: set sw=4 sts=4 ts=4 et tw=80 :
 
+# latest = current stable versions
+# next = all testing versions unmasked
+IMAGE_VERSION=${1:-latest}
+
+if [[ ${IMAGE_VERSION} == "latest" ]]; then
+    DEPENDENCY_VERSIONS="stable"
+elif [[ ${IMAGE_VERSION} == "next" ]]; then
+    DEPENDENCY_VERSIONS="testing broken"
+else
+    echo "Unknown image version, use 'latest' or 'next'"
+    exit 1
+fi
+
 docker build \
     --no-cache \
     --rm \
     --pull \
-    --tag paludis/exherbo-gcc \
+    --build-arg DEPENDENCY_VERSIONS="${DEPENDENCY_VERSIONS}" \
+    --tag paludis/exherbo-gcc:${IMAGE_VERSION} \
     .
