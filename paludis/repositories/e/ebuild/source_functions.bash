133e5d93d1891461af9a9c0c393536383107ba7e
Mihai Moldovan <ionic@ionic.de>
2020-09-17 01:25:36 +0200

    eapi 0/exheres-0 / source functions: fix saveenv.
    So far, paludis used "set" to dump all shell and environment variables
    out to a file (c.f., "environment.bz2") and then a small utility called
    "print_exports", which issued "export" calls for exported environment
    variables.
    
    It also had a way to rewrite environment dump files that have been
    created using "declare -p" into its own expected form.
    
    This worked more or less fine, but is considered a legacy environment
    dump form. Portage and pkgcore switched to the "declare -p" form a long
    time ago and recently, not using it started causing trouble.
    
    For instance, the go-module eclass started using bash associative
    arrays.
    
    These are a bit weird in that they can't be created without a declare
    call. By default, bash assumes normal arrays when it sees that syntax
    and the only way to change that is using the "declare" or "typeset"
    builtin.
    
    Crucially, this also means that you cannot dump and reapply associative
    arrays with the set builtin.
    
    Other complex variables with additional attributes (such as integer-type
    or automatic lower and uppercase conversion variables) can behave
    wrongly when being used later on without applying the attributes.
    
    Hence, we'll also switch to the more sophisticated declare/typeset tool
    and let it print out variables (attributes, names, values) and later on
    functions.
    
    Additionally, avoid subshells (slow) and use compound commands instead
    and correctly quote the target file.
    
    We can now also drop the --rewrite-for-declare flag to
    ebuild_safe_source, since going forward, our environment dump files will
    all contain "declare" calls anyway.
    
    For this to work correctly, we'll have to modify the variable name
    detection/skip code to handle lines starting with "declare <options>"
    correctly and make sure that variables are NOT set locally within the
    shell function only. For the latter issue, make sure to add -g to any
    declare calls when applying the dumped data.
    
    Lastly, properly document the ebuild_safe_source function. It's
    complicated and took me a lot of time to understand how it works
    multiple times in a row. Better explain it in some easily understandable
    textual form.
    
    Fixes: https://github.com/MageSlayer/paludis-gentoo-patches/issues/26

--- paludis/repositories/e/ebuild/source_functions.bash
+++ paludis/repositories/e/ebuild/source_functions.bash
@@ -36,6 +36,8 @@ ebuild_need_extglob()
 
 ebuild_safe_source()
 {
+    # Set a list of variable names (and globs) that we want to filter out as positional arguments.
+    # We'll use those in a fake signal handler later.
     set -- "${@}" '[^a-zA-Z_]*' '*[^a-zA-Z0-9_]*' \
         EUID PPID UID FUNCNAME GROUPS SHELLOPTS BASHOPTS BASHPID IFS PWD \
         'BASH_@(ARGC|ARGV|LINENO|SOURCE|VERSINFO|REMATCH)' \
@@ -44,17 +46,78 @@ ebuild_safe_source()
         PALUDIS_IGNORE_PIVOT_ENV_FUNCTIONS PALUDIS_IGNORE_PIVOT_ENV_VARIABLES \
         PALUDIS_PIPE_COMMAND_READ_FD PALUDIS_PIPE_COMMAND_WRITE_FD
 
+    # Clear the debug trap.
     trap DEBUG
+
+    # Make sure that DEBUG traps are passed down to other processes as well.
     set -T
+
+    # Enable some special handling of the DEBUG trap.
+    # For instance, we skip executing commands if the debug trap exits with a non-zero status.
     shopt -s extdebug
-    trap "[[ \${BASH_COMMAND%%=*} == ?(*[[:space:]])!($(IFS='|'; [[ ${1} == --rewrite-for-declare ]] && shift; shift; echo "${*}")) ||
+
+    # Main filtering function, implemented as a DEBUG trap.
+    # This will execute before every command. We can filter out commands by returning non-zero here.
+    # A return value of 2 will halt the complete source statement. (This is not used here, but could
+    # come in handy in case of errors?)
+    # Understanding the filtering is a bit complicated:
+    #   - First, we check for and drop preceding "declare" calls and their options by:
+    #     o dropping everything past the first equal sign (this should leave us with a potential
+    #       declare call, its options and the variable name)
+    #     o dropping everything after the first space character and checking if that evaluates to a
+    #       "declare" call
+    #     o if it is, extract the variable name by dropping everything up to the last space
+    #       character. This should drop the declare call and its options up until the variable name.
+    #       This COULD create problems if the variable name contains one or multiple space
+    #       characters, but we'll ignore that since, while such environment variables would
+    #       generally be syntactically valid, they are not valid bash identifiers.
+    #  - Then, check if the variable name matches any of our positional parameters. To do this, we
+    #    concatenate them in a subshell(!) by settings the IFS variable to a pipe character and
+    #    printing out all parameters through the ${*} shell variable.
+    #    As an example, if the positional parameters are 'a', 'b', 'c' and 'd', this will generate
+    #    this string: 'a|b|c|d'. Together with the extglob feature and a negation group, we can
+    #    filter out the command by returning a non-zero exit value via the test utility.
+    #  - Likewise, drop everything up until the first space character and check if that matches
+    #    either a key-value pair (i.e., variable assignment) or the export or declare statements.
+    #
+    # Eventually, we might also want to include the "typeset" builtin, which is an alias for
+    # "declare".
+    trap "varname=\"\${BASH_COMMAND%%=*}\";
+          [[ \${varname%%[[:space:]]*} == 'declare' ]] && {
+              \$(: 'Skip over \"declare\" and options');
+              varname=\"\${varname##*[[:space:]]}\";
+          };
+          [[ \${varname} == ?(*[[:space:]])!($(IFS='|'; shift; echo "${*}")) ||
               \${BASH_COMMAND%%[[:space:]]*} != @(*=*|export|declare) ]]" DEBUG
 
-    if [[ ${1} == --rewrite-for-declare ]]; then
-        ( source "${2}" && set >"${2}" && print_exports >>"${2}" ) && source "${2}"
-    else
-        source "${1}"
-    fi
+    # Sourcing a file with variables defined via declare or typeset in a function
+    # will make the resulting variables local to the function.
+    # We'll need them globally, though, so rewrite the statements to always include
+    # a -g parameter.
+    # We don't want to do this when creating the dump file, since portage doesn't
+    # do it either and it actually makes sense to keep it pristine.
+    #
+    # To modify the original data, we have two options:
+    #   - read in the data, modify it, write out to a tempfile and source the
+    #     tempfile.
+    #   - modify the data in place, then source it.
+    #   - read in the data, modify and apply it directly.
+    #
+    # We'd like to avoid the first option, since that would require creating
+    # temporary files.
+    # The second option requires a GNU sed binary since in-place modification
+    # is a bit complicated.
+    # The third option requires switching from the source builtin to the eval
+    # builtin, since only files can be source'd.
+    # Additionally, even if source supported reading and applying from stdin,
+    # we couldn't use pipes or subshells since that would create a new process
+    # space. Applying any new variables in there wouldn't affect the parent,
+    # but we actually intend to do exactly that.
+    # Now, the "issue" with eval is that we won't be able to use the special
+    # "call return while executing the source builtin if the DEBUG trap's return
+    # value was 2" property, so we'll settle on option two.
+    sed -i -e 's/^declare -/declare -g -/' "${1}" && source "${1}"
+
     eval "trap DEBUG; shopt -u extdebug; set +T; return ${?}"
 }
 ebuild_need_extglob ebuild_safe_source
