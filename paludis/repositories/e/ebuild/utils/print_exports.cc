e349d6cd422c604a8cd6211e8e5835693ede4dfd
Denis Golovan <denisgolovan@yandex.ru>
2020-12-28 19:59:39 +0200

    Revert "utils/print_exports: drop."
    This reverts commit 1cbf53ab101065fbc004418545142a6d239c557e.

---
+++ paludis/repositories/e/ebuild/utils/print_exports.cc
@@ -0,0 +1,34 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2007 David Leverton
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <string>
+#include <iostream>
+
+extern char ** environ;
+
+int
+main()
+{
+    for (char ** it = environ; nullptr != *it; ++it)
+    {
+        std::string str(*it);
+        std::cout << "export " << str.substr(0, str.find('=')) << std::endl;
+    }
+}
+
