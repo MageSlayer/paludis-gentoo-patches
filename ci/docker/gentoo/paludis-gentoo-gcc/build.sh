f957e62d2c52ef642ba7583a4ff25f572da59e56
Marvin Schmidt <marv@exherbo.org>
2021-02-07 11:39:48 +0100

    ci: Remove Gentoo for now

--- ci/docker/gentoo/paludis-gentoo-gcc/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# vim: set sw=4 sts=4 ts=4 et tw=80 :
-
-docker build --no-cache --rm --pull -t paludis/gentoo-gcc .
--- ci/docker/update-images.sh
+++ ci/docker/update-images.sh
@@ -4,5 +4,3 @@
 docker build -t paludis/exherbo-gcc exherbo/paludis-exherbo-gcc
 docker build -t paludis/exherbo-clang exherbo/paludis-exherbo-clang
 
-docker build -t paludis/gentoo-gcc gentoo/paludis-gentoo-gcc
-
