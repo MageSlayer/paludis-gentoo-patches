MageSlayer/paludis-gentoo-patches

commit 3cac76bba19afa2bd70307024d6f6f6485526d29

exheres-0: Use esandbox instead of sydboxcmd

paludis/repositories/e/ebuild/exheres-0/pkg_config.bash
@@ -36,7 +36,7 @@ exheres_internal_config()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${ROOT%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
 
     if hasq "config" ${SKIP_FUNCTIONS} ; then
@@ -49,7 +49,7 @@ exheres_internal_config()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/pkg_postinst.bash
@@ -36,7 +36,7 @@ exheres_internal_postinst()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${ROOT%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
 
     if hasq "postinst" ${SKIP_FUNCTIONS} ; then
@@ -49,7 +49,7 @@ exheres_internal_postinst()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/builtin_loadenv.bash
@@ -30,7 +30,7 @@ exheres_internal_loadenv()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${PALUDIS_LOADSAVEENV_DIR%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${PALUDIS_LOADSAVEENV_DIR}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${PALUDIS_LOADSAVEENV_DIR}"
     fi
 
     if hasq "loadenv" ${SKIP_FUNCTIONS} ; then
@@ -43,7 +43,7 @@ exheres_internal_loadenv()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${PALUDIS_LOADSAVEENV_DIR}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${PALUDIS_LOADSAVEENV_DIR}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/builtin_saveenv.bash
@@ -28,7 +28,7 @@ exheres_internal_saveenv()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${PALUDIS_LOADSAVEENV_DIR%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${PALUDIS_LOADSAVEENV_DIR}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${PALUDIS_LOADSAVEENV_DIR}"
     fi
 
 
@@ -42,7 +42,7 @@ exheres_internal_saveenv()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${PALUDIS_LOADSAVEENV_DIR}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${PALUDIS_LOADSAVEENV_DIR}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/pkg_nofetch.bash
@@ -46,7 +46,7 @@ exheres_internal_nofetch()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${FETCHEDDIR}"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${FETCHEDDIR}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${FETCHEDDIR}"
     fi
 
     if hasq "nofetch" ${SKIP_FUNCTIONS} ; then
@@ -59,7 +59,7 @@ exheres_internal_nofetch()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${FETCHEDDIR}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${FETCHEDDIR}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/pkg_postrm.bash
@@ -36,7 +36,7 @@ exheres_internal_postrm()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${ROOT%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
 
     if hasq "postrm" ${SKIP_FUNCTIONS} ; then
@@ -49,7 +49,7 @@ exheres_internal_postrm()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${ROOT}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/pkg_preinst.bash
@@ -36,7 +36,7 @@ exheres_internal_preinst()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${ROOT%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
 
     if hasq "preinst" ${SKIP_FUNCTIONS} ; then
@@ -49,7 +49,7 @@ exheres_internal_preinst()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${ROOT}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/pkg_prerm.bash
@@ -36,7 +36,7 @@ exheres_internal_prerm()
     local old_sandbox_write="${SANDBOX_WRITE}"
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${SANDBOX_WRITE+${SANDBOX_WRITE}:}${ROOT%/}/"
-        sydboxcheck >/dev/null 2>&1 && addwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
     fi
 
     if hasq "prerm" ${SKIP_FUNCTIONS} ; then
@@ -49,7 +49,7 @@ exheres_internal_prerm()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        sydboxcheck >/dev/null 2>&1 && rmwrite "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${ROOT}"
     fi
     true
 }

commit 27c4db704588d4026d86e6a01f1955cb49828147

exheres-0: correct some unfortunate typos
Fixes: 3cac76bba ("exheres-0: Use esandbox instead of sydboxcmd")

paludis/repositories/e/ebuild/exheres-0/pkg_config.bash
@@ -49,7 +49,7 @@ exheres_internal_config()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${ROOT}"
     fi
     true
 }

paludis/repositories/e/ebuild/exheres-0/pkg_postinst.bash
@@ -49,7 +49,7 @@ exheres_internal_postinst()
 
     if [[ -z "${PALUDIS_DO_NOTHING_SANDBOXY}" ]]; then
         SANDBOX_WRITE="${old_sandbox_write}"
-        esandbox check >/dev/null 2>&1 && esandbox allow "${ROOT}"
+        esandbox check >/dev/null 2>&1 && esandbox disallow "${ROOT}"
     fi
     true
 }