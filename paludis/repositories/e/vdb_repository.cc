MageSlayer/paludis-gentoo-patches

commit 503a820e92affef4ec1865679cdc904ac4f7db5c

Control over dep rewrites too

paludis/repositories/e/vdb_repository.cc
@@ -1246,7 +1246,7 @@ namespace
         }
     };
 
-    void rewrite_dependencies(
+    bool rewrite_dependencies(
             const FSEntry & f,
             const std::tr1::shared_ptr<const MetadataSpecTreeKey<DependencySpecTree> > & key,
             const DepRewrites & rewrites)
@@ -1255,10 +1255,17 @@ namespace
         key->value()->root()->accept(v);
         if (v.changed)
         {
-            std::cout << "    Rewriting " << f << std::endl;
-            SafeOFStream ff(f);
-            ff << v.str.str() << std::endl;
+            if ("yes" == getenv_with_default("PALUDIS_CARRY_OUT_UPDATES", ""))
+            {
+                std::cout << "    Rewriting " << f << std::endl;
+                SafeOFStream ff(f);
+                ff << v.str.str() << std::endl;
+            }
+            else
+                std::cout << "    Would rewrite " << f << std::endl;
         }
+
+        return v.changed;
     }
 }
 
@@ -1392,8 +1399,7 @@ VDBRepository::perform_updates()
 
     try
     {
-        if ((! moves.empty()) || (! slot_moves.empty()))
-            std::cout << std::endl;
+        std::cout << std::endl;
 
         if (! moves.empty())
         {
@@ -1476,23 +1482,31 @@ VDBRepository::perform_updates()
         {
             std::cout << "Updating installed package dependencies" << std::endl;
 
+            bool rewrite_done(false);
             const std::tr1::shared_ptr<const PackageIDSequence> ids((*_imp->params.environment())[selection::AllVersionsSorted(
                         generator::InRepository(name()))]);
             for (PackageIDSequence::ConstIterator i(ids->begin()), i_end(ids->end()) ;
                     i != i_end ; ++i)
             {
                 if ((*i)->build_dependencies_key())
-                    rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->build_dependencies_key()->raw_name(),
+                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->build_dependencies_key()->raw_name(),
                             (*i)->build_dependencies_key(), dep_rewrites);
                 if ((*i)->run_dependencies_key())
-                    rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->run_dependencies_key()->raw_name(),
+                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->run_dependencies_key()->raw_name(),
                             (*i)->run_dependencies_key(), dep_rewrites);
                 if ((*i)->post_dependencies_key())
-                    rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->post_dependencies_key()->raw_name(),
+                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->post_dependencies_key()->raw_name(),
                             (*i)->post_dependencies_key(), dep_rewrites);
                 if ((*i)->suggested_dependencies_key())
-                    rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->suggested_dependencies_key()->raw_name(),
+                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->value() / (*i)->suggested_dependencies_key()->raw_name(),
                             (*i)->suggested_dependencies_key(), dep_rewrites);
+            }
+
+            if ("yes" != getenv_with_default("PALUDIS_CARRY_OUT_UPDATES", ""))
+            {
+                std::cout << "Some installed packages have dependencies that need rewriting for package" << std::endl;
+                std::cout << "moves. See the Paludis FAQ for how to proceed." << std::endl;
+                std::cout << std::endl;
             }
         }
     }

commit 809cb0d1f112ac91cb0e4a7b82f56c74d1b65be5

repositories: e: vdb_repository: Remove unused variable (NFC)
The `rewrite_done` variable was introduced in commit 503a820e9 ("Control
over dep rewrites too"), but wasn't used until commit 9990588a6 ("Don't
show the faq message if there're no rewrites to do") where it was used
in order to show the FAQ message only if rewrites were done:

  if ((rewrite_done) && ("yes" != getenv_with_default("PALUDIS_CARRY_OUT_UPDATES", "")))
    // show FAQ message

Commit 3993ba5b8 ("Enable updates by default") then removed the
PALUDIS_CARRY_OUT_UPDATES logic and enabled updates by default, but kept
the `rewrite_done` variable.

This commit removes the variable and changes the `rewrite_dependencies`
function back to returning `void` as it was before commit 503a820e9

Fixes: 3993ba5b8 ("Enable updates by default")

paludis/repositories/e/vdb_repository.cc
@@ -1036,7 +1036,7 @@ namespace
         }
     };
 
-    bool rewrite_dependencies(
+    void rewrite_dependencies(
             const FSPath & f,
             const std::shared_ptr<const MetadataSpecTreeKey<DependencySpecTree> > & key,
             const DepRewrites & rewrites)
@@ -1049,8 +1049,6 @@ namespace
             SafeOFStream ff(f, -1, true);
             ff << v.str.str() << std::endl;
         }
-
-        return v.changed;
     }
 }
 
@@ -1314,20 +1312,19 @@ VDBRepository::perform_updates()
         {
             std::cout << std::endl << "Updating installed package dependencies" << std::endl;
 
-            bool rewrite_done(false);
             const std::shared_ptr<const PackageIDSequence> ids((*_imp->params.environment())[selection::AllVersionsSorted(
                         generator::InRepository(name()))]);
             for (PackageIDSequence::ConstIterator i(ids->begin()), i_end(ids->end()) ;
                     i != i_end ; ++i)
             {
                 if ((*i)->build_dependencies_key())
-                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->parse_value() / (*i)->build_dependencies_key()->raw_name(),
+                    rewrite_dependencies((*i)->fs_location_key()->parse_value() / (*i)->build_dependencies_key()->raw_name(),
                             (*i)->build_dependencies_key(), dep_rewrites);
                 if ((*i)->run_dependencies_key())
-                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->parse_value() / (*i)->run_dependencies_key()->raw_name(),
+                    rewrite_dependencies((*i)->fs_location_key()->parse_value() / (*i)->run_dependencies_key()->raw_name(),
                             (*i)->run_dependencies_key(), dep_rewrites);
                 if ((*i)->post_dependencies_key())
-                    rewrite_done |= rewrite_dependencies((*i)->fs_location_key()->parse_value() / (*i)->post_dependencies_key()->raw_name(),
+                    rewrite_dependencies((*i)->fs_location_key()->parse_value() / (*i)->post_dependencies_key()->raw_name(),
                             (*i)->post_dependencies_key(), dep_rewrites);
             }
 