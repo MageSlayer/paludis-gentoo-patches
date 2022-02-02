bdcb27588898bc5588ecb5a06c5eefbe728d6a8d
Paul Zander <negril.nx@gmail.com>
2022-02-03 00:13:42 +0100

    tests: add EAPI=8 tests.
    This adds the following tests:
      - bash compat 5.0+
      - econf passes --datarootdir="${EPREFIX}/usr/share" and
        --disable-static by default
      - doconfd, doenvd, doheader not influenced by insopts and doinitd not
        influenced by exeopts any longer
      - dosym -r for relative symlinks
      - fetch and mirror restriction overrides via fetch+/mirror+ SRC_URI
        prefixes
      - hasq, hasv and useq banned
      - new IDEPEND variable, RDEPEND-equivalent to BDEPEND and DEPEND
      - PATCHES variable/array does not accept options any longer
      - PROPERTIES and RESTRICT now accumulate through eclasses
      - new optional test_network value for PROPERTIES, overriding test
        restrictions
      - 7-Zip, LHA and RAR formats dropped from base unpack
      - second argument to usev function to override output value
      - pkg_* phases start in empty directory
    
    Backported to old testing system and slightly modified (mostly by adding
    quoting) by Mihai Moldovan <ionic@ionic.de>.

---
+++ paludis/repositories/e/e_repository_TEST_8_setup.sh
@@ -0,0 +1,381 @@
+#!/usr/bin/env bash
+# vim: set ft=sh sw=4 sts=4 et :
+
+set -e
+
+mkdir e_repository_TEST_8_dir
+cd e_repository_TEST_8_dir
+
+mkdir -p root/etc
+
+mkdir -p vdb
+touch vdb/THISISTHEVDB
+
+mkdir -p build
+
+mkdir -p distdir
+
+for format in 7z lha lzh rar; do
+    touch "distdir/test.${format}"
+done
+
+mkdir -p repo/{profiles/profile,metadata,eclass}
+cd repo
+echo "test-repo" >> profiles/repo_name
+cat <<'END' > profiles/arch.list
+amd64
+arm
+arm64
+x86
+
+# Prefix keywords
+amd64-linux
+arm-linux
+arm64-linux
+x86-linux
+END
+
+echo "cat" >> profiles/categories
+cat <<'END' > profiles/profile/make.defaults
+ARCH="cheese"
+USERLAND="GNU"
+KERNEL="linux"
+LIBC="glibc"
+CHOST="i286-badger-linux-gnu"
+LINGUAS="enabled_en enabled_en_GB enabled_en_GB@UTF-8"
+USE_EXPAND="LINGUAS USERLAND"
+USE_EXPAND_UNPREFIXED="ARCH"
+USE_EXPAND_IMPLICIT="USERLAND ARCH"
+USE_EXPAND_VALUES_USERLAND="GNU"
+USE_EXPAND_VALUES_ARCH="cheese otherarch"
+IUSE_IMPLICIT="build"
+END
+
+# bash 5.0 is now sanctioned
+# PALUDIS_BASH_COMPAT
+mkdir -p "cat/bash-compat"
+cat <<'END' > cat/bash-compat/bash-compat-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="spork"
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+S="${WORKDIR}"
+
+pkg_pretend() {
+    echo "${PALUDIS_BASH_COMPAT}"
+}
+END
+
+# econf passes --build, --target, --with-sysroot
+# --datarootdir new econf-passed option
+# --disable-static new econf-passed option
+mkdir -p "cat/econf-added-options"
+cat <<'END' > cat/econf-added-options/econf-added-options-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="spork"
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+S="${WORKDIR}"
+
+src_prepare(){
+    cat <<'EOF' > configure
+#!/bin/sh
+
+if echo "$@" | grep -q 'help' ; then
+    echo --datarootdir
+    echo --disable-static
+    exit 0
+fi
+
+if ! echo "$@" | grep -q datarootdir="${EPREFIX}"/usr/share ; then
+    echo --datarootdir missing
+    exit 1
+fi
+
+if ! echo "$@" | grep -q 'disable-static' ; then
+    echo --disable-static missing
+    exit 1
+fi
+
+exit 0
+EOF
+
+    chmod +x configure
+}
+END
+
+# doconfd no longer affected by insopts
+# doenvd no longer affected by insopts
+# doheader no longer affected by insopts
+# doinitd no longer affected by exeopts
+mkdir -p "cat/changed-opts"
+cat <<'END' > cat/changed-opts/changed-opts-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="spork"
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+S="${WORKDIR}"
+
+src_install() {
+   insopts --foo
+   exeopts --bar
+   echo foo > foo
+   doconfd foo
+   doenvd foo
+   doheader foo
+   doinitd foo
+}
+END
+
+# dosym -r allows creating relative symlinks
+mkdir -p "cat/dosym-rel"
+cat <<'END' > cat/dosym-rel/dosym-rel-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="spork"
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+S="${WORKDIR}"
+
+src_install() {
+    echo bar > bar
+    dobin bar
+    dosym ${D}/usr/bin/bar /usr/bin/foo
+    [[ "$(readlink "${D}/usr/bin/bar")" == "foo" ]] || die 'link wrong'
+}
+END
+
+# fetch+ SRC_URI prefix to override fetch restriction
+# mirror+ SRC_URI prefix to override mirror restriction
+# TODO
+mkdir -p "cat/fetch-restrictions"
+cat <<'END' > cat/fetch-restrictions/fetch-restrictions-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI="fetch+test.7z mirror+test.rar"
+SLOT="0"
+IUSE="spork"
+LICENSE="GPL-2"
+KEYWORDS="test"
+RESTRICT="fetch mirror"
+
+END
+
+# hasq banned
+# hasv banned
+# useq banned
+mkdir -p "cat/banned-functions"
+cat <<'END' > cat/banned-functions/banned-functions-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="spork"
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+S="${WORKDIR}"
+
+pkg_pretend() {
+    [[ -n "$(declare -F hasq)" ]]  && die 'hasq is banned'
+    [[ -n "$(declare -F hasv)" ]]  && die 'hasv is banned'
+    [[ -n "$(declare -F useq)" ]]  && die 'useq is banned'
+}
+END
+
+# IDEPEND new dependency type for pkg_postinst deps
+mkdir -p "cat/changed-vars"
+cat <<'END' > cat/changed-vars/changed-vars-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE=""
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+pkg_pretend() {
+for var in IDEPEND; do
+  [ -z "${!var+x}" ] && echo "${var} has been added and should be set"
+done
+}
+END
+
+# PATCHES no longer permits specifying options (paths only)
+mkdir -p "cat/patches-no-opts"
+cat <<'END' > cat/patches-no-opts/patches-no-opts-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE=""
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+PATCHES=( -p0 "${FILESDIR}"/${P}-foo.patch )
+
+S="${WORKDIR}"
+
+src_unpack() {
+    echo first >file || die
+}
+
+src_compile() {
+    [[ "$(< file)" == 'second' ]] || die 'file wrong'
+}
+END
+
+mkdir -p "cat/patches-no-opts/files" || exit 1
+cat << 'END' > cat/patches-no-opts/files/patches-no-opts-8-foo.patch || exit 1
+diff --git a/file b/file
+index 9c59e24..e019be0 100644
+--- a/file
++++ b/file
+@@ -1 +1 @@
+-first
++second
+END
+
+# PROPERTIES now accumulated across eclasses
+# RESTRICT now accumulated across eclasses
+# TODO
+cat <<'END' > eclass/foo.eclass
+PROPERTIES="FOO"
+RESTRICT="FOO"
+END
+cat <<'END' > eclass/bar.eclass
+PROPERTIES="BAR"
+RESTRICT="BAR"
+END
+
+mkdir -p "cat/accumulated-vars"
+cat <<'END' > cat/accumulated-vars/accumulated-vars-8.ebuild
+EAPI="8"
+inherit foo bar
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE=""
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+pkg_pretend() {
+    [[ "${PROPERTIES}" == *FOO* ]] || die 'failed to accumulate PROPERTIES'
+    [[ "${PROPERTIES}" == *BAR* ]] || die 'failed to accumulate PROPERTIES'
+    [[ "${RESTRICT}" == *FOO* ]] || die 'failed to accumulate RESTRICT'
+    [[ "${RESTRICT}" == *BAR* ]] || die 'failed to accumulate RESTRICT'
+}
+END
+
+# test_network new PROPERTIES value for tests requiring Internet
+mkdir -p "cat/test-network"
+cat <<'END' > cat/test-network/test-network-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="flag"
+LICENSE="GPL-2"
+KEYWORDS="test"
+PROPERTIES="test_network"
+
+pkg_pretend() {
+    die 'not implemented'
+}
+src_prepare() {
+    die 'not implemented'
+}
+END
+
+# unpack no longer supports 7-Zip, LHA and RAR formats
+# TODO
+mkdir -p "cat/unpack-formats-removed"
+cat <<'END' > cat/unpack-formats-removed/unpack-formats-removed-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI="test.7z test.lha test.lzh test.rar"
+SLOT="0"
+IUSE="flag"
+LICENSE="GPL-2"
+KEYWORDS="test"
+S="${WORKDIR}"
+
+src_unpack() {
+    export PALUDIS_UNPACK_UNRECOGNISED_IS_FATAL=yes
+    for format in 7z lha lzh rar; do
+        nonfatal unpack test.${format} 2> /dev/null > /dev/null
+        [[ '0' -eq "$?" ]] && die "${format} should not be unpacked"
+    done
+
+}
+END
+
+# updates filenames no longer have to follow nQ-yyyy style
+# TODO
+
+# usev accepts second argument to override output value
+mkdir -p "cat/usev-second-arg"
+cat <<'END' > cat/usev-second-arg/usev-second-arg-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE="flag"
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+pkg_pretend() {
+    [[ "$(usev flag true)" == "true" ]] || die 'usev second arg no supported'
+}
+END
+
+# working directory pkg_* phases now start in an empty directory
+# TODO
+mkdir -p "cat/pkg-empty-dir"
+cat <<'END' > cat/pkg-empty-dir/pkg-empty-dir-8.ebuild
+EAPI="8"
+DESCRIPTION="The Description"
+HOMEPAGE="http://example.com/"
+SRC_URI=""
+SLOT="0"
+IUSE=""
+LICENSE="GPL-2"
+KEYWORDS="test"
+
+pkg_config() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_info() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_nofetch() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_postinst() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_postrm() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_preinst() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_prerm() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_setup() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+pkg_pretend() { [[ "$(find . | wc -l)" -gt '1' ]] && die "$(pwd) not empty"; }
+END
