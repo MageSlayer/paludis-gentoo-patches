23efff4e314b78e4e69323889c04c2f14d2a3bc5
Mihai Moldovan <ionic@ionic.de>
2022-02-08 09:34:57 +0100

    tests: execute pretend action instead of install action in vers_* test in EAPI=7 tests.
    Installing the package but later checking the pretend action status
    naturally will not work.

--- paludis/repositories/e/e_repository_TEST_7.cc
+++ paludis/repositories/e/e_repository_TEST_7.cc
@@ -338,7 +338,7 @@ TEST(ERepository, InstallEAPI7)
                                 &env, { })), nullptr, { }))]->last());
         ASSERT_TRUE(bool(id));
         EXPECT_EQ("7", visitor_cast<const MetadataValueKey<std::string> >(**id->find_metadata("EAPI"))->parse_value());
-        id->perform_action(action);
+        id->perform_action(pretend_action);
         EXPECT_TRUE(! pretend_action.failed());
     }
 
