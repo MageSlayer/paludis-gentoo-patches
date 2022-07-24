d38479183334685a8b3fbe603f429362e161a2f1
Marvin Schmidt <marv@exherbo.org>
2022-07-24 12:51:19 +0200

    ci: configure-paludis: Use Ruby 3.1 by default

--- ci/configure-paludis.sh
+++ ci/configure-paludis.sh
@@ -3,7 +3,7 @@ DISTRIBUTION=${2}
 COMPILER=${3}
 BUILD_TYPE=${4:-debug}
 
-[[ -z ${RUBY_VERSION} ]] && RUBY_VERSION=3.0
+[[ -z ${RUBY_VERSION} ]] && RUBY_VERSION=3.1
 
 myconf=()
 if [[ ${DISTRIBUTION} == "exherbo" ]]; then
