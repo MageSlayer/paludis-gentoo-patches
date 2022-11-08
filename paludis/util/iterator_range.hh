MageSlayer/paludis-gentoo-patches

commit 0161a0f68496682d03c408c5e8c620b620a31611

util: add IteratorRange utility
This is a utility class meant to provide a helper which permits returning a
range for iteration via a named pair of iterators.  It serves as an adapter
for creating a C++11 friendly interface for iteration.

paludis/util/iterator_range.hh
@@ -1,0 +1,78 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+/*
+ * Copyright (c) 2014 Saleem Abdulrasool
+ *
+ * This file is part of the Paludis package manager. Paludis is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * Paludis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef PALUDIS_GUARD_PALUDIS_UTIL_ITERATOR_RANGE_HH
+#define PALUDIS_GUARD_PALUDIS_UTIL_ITERATOR_RANGE_HH 1
+
+#include <iterator>
+#include <utility>
+
+namespace paludis
+{
+    template <typename Iterator_>
+    class IteratorRange
+    {
+        private:
+            Iterator_ _begin, _end;
+
+        public:
+            // TODO(compnerd) use SFINAE to ensure that the container's
+            // iterators match the range's iterator
+            template <typename Container_>
+            IteratorRange(Container_ && container)
+                : _begin(std::begin(container)), _end(std::end(container))
+            {
+            }
+
+            IteratorRange(Iterator_ begin, Iterator_ end)
+                : _begin(std::move(begin)), _end(std::move(end))
+            {
+            }
+
+            Iterator_ begin() const
+            {
+                return _begin;
+            }
+
+            Iterator_ end() const
+            {
+                return _end;
+            }
+
+            size_t size() const
+            {
+                return std::distance(_end, _begin);
+            }
+    };
+
+    template <typename Iterator_>
+    IteratorRange<Iterator_> make_range(Iterator_ begin, Iterator_ end)
+    {
+        return IteratorRange<Iterator_>(std::move(begin), std::move(end));
+    }
+
+    template <typename Iterator_>
+    IteratorRange<Iterator_> make_range(std::pair<Iterator_, Iterator_> range)
+    {
+        return IteratorRange<Iterator_>(std::move(range.first),
+                                        std::move(range.second));
+    }
+}
+
+#endif
+

paludis/util/files.m4
@@ -42,6 +42,7 @@ add(`fs_stat',                           `hh', `cc', `fwd', `gtest', `testscript
 add(`graph',                             `hh', `cc', `fwd', `impl', `gtest')
 add(`hashes',                            `hh', `cc', `gtest')
 add(`iterator_funcs',                    `hh', `gtest')
+add(`iterator_range',                    `hh')
 add(`indirect_iterator',                 `hh', `fwd', `impl', `gtest')
 add(`is_file_with_extension',            `hh', `cc', `se', `gtest', `testscript')
 add(`join',                              `hh', `gtest')

commit d9eb24cac6be7216dbf46ed974f77669be1b5f77

util: iterator_range: Fix size()
The size() method returned wrong results because it had begin and end
iterators switched. It went unnoticed because we neither had any test
nor user for it so far

Fixes: 0161a0f68496 ("util: add IteratorRange utility")

paludis/util/iterator_range.hh
@@ -59,7 +59,7 @@ namespace paludis
 
             size_t size() const
             {
-                return std::distance(_end, _begin);
+                return std::distance(_begin, _end);
             }
     };
 